4c08f91f9beac716aa5b425baa8a1a4593a21793
AMN69 <martinez.andreu@gmail.com>
2022-01-02 20:00:49 +0100

    20220202 testing oo javascript netninja

--- objects.js
+++ objects.js
@@ -1,3 +1,12 @@
 class User {
-    
+    constructor(email, name) {
+        this.email = email
+        this.name = name
+    }
 }
+
+var userOne = new User('pepe@pepe.com', 'Pepe')
+var userTwo = new User('jose@jose.com', 'José')
+
+console.log(userOne)
+console.log(userTwo)
